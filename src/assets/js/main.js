f9fff31d28556010ed8e8cd55289832b12c3999e
Diandra <leao.diandra@gmail.com>
2021-03-14 17:16:58 -0300

    menu mobile

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,3 +6,14 @@
 
 import css from '../css/reset.css';
 import scss from '../css/main.scss';
+
+// menu
+
+let menuBtn = document.getElementById("mobile-menu");
+let menuMob = document.querySelector(".header");
+menuBtn.addEventListener('click', e => {
+	menuBtn.classList.toggle("open");
+	menuMob.classList.toggle("open-mob");
+});
+
+

ac298bb3a0557dc533252681b9b73eda18208792
Diandra <leao.diandra@gmail.com>
2021-06-28 16:08:08 -0300

    instagram e contato mobile

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,13 +7,30 @@
 import css from '../css/reset.css';
 import scss from '../css/main.scss';
 
-// menu
+import {tns} from 'tiny-slider/src/tiny-slider';
 
+// menu
 let menuBtn = document.getElementById("mobile-menu");
 let menuMob = document.querySelector(".header");
 menuBtn.addEventListener('click', e => {
 	menuBtn.classList.toggle("open");
 	menuMob.classList.toggle("open-mob");
+});
+
+
+var slider = tns({
+	container: '#instagram__carousel',
+	items: 1,
+	center: true,
+	loop: true,
+	nav: true,
+	navPosition: 'bottom',
+	controls: true,
+	autoplay: true,
+	speed: 600,
+	autoplayTimeout: 3000,
+	autoplayButtonOutput: false,
+	controlsText: ['<', '>']
 });
 
 
